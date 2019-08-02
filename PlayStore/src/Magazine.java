aadf7a4f85d7983324975b403ba2182e14c75910
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2019-08-02 15:36:51 +1000

    Update Magazine.java

--- PlayStore/src/Magazine.java
+++ PlayStore/src/Magazine.java
@@ -1,40 +1,41 @@
 import java.util.*;
 
-//create a class Magazine
 class Magazine extends Publication {
-	// subclass instance variables
-	int volume; 
+	private int volume;
 	
 	public Magazine(String contentID, String name, int downNum, double price, ArrayList<Comment> comments,
 			String publisher, int numPage, int volume) {
 		super(contentID, name, downNum, price, comments, publisher, numPage);
-		this.volume = volume;	
+		this.setVolume(volume);
 	}
 	
-	// overloading + set up the initial value for Magazine
 	public Magazine(String contentID, String name, int downNum, double price, ArrayList<Comment> comments,
 			String publisher, int numPage) {
 		this(contentID, name, downNum, price, comments, publisher, numPage, 0);
 	}
 	
-	public Magazine(String contentID, String name, double price, String publisher, 
-			int numPage, int volume) {
+	public Magazine(String contentID, String name, double price, String publisher, int numPage, int volume) {
 		this(contentID, name, 0, price, new ArrayList<Comment>(), publisher, numPage, volume);
 	}
 	
-	@Override
-	public void addReview(Comment comment) {	
-		this.comments.add(comment); // add comment to Magazine
-	}
-
-	@Override
-	public ArrayList<Comment> showComments() {
-        return this.comments; // return the list of comments for Magazine		
+	public void addReview(Comment comment) {
+		this.getComment().add(comment);
 	}
 	
-	@Override
+	public ArrayList<Comment> showComments() {
+		return this.getComment();
+	}
+	
 	public void removeReview(Comment comment) {
-		this.comments.remove(comment); // remove comment from Magazine
-	}	
-}
+		this.getComment().remove(comment);
+	}
 
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int newVolume) {
+		this.volume = newVolume;
+	}
+	
+}
