5ed669f775f3728d5b867ddaabc58b892836ddb6
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2019-08-02 15:37:17 +1000

    Update PlayStore.java

--- PlayStore/src/PlayStore.java
+++ PlayStore/src/PlayStore.java
@@ -1,47 +1,36 @@
 import java.util.*;
 
 public class PlayStore {
-	// instance variables with default modifiers
-	
 	HashMap<String, Content> contents;
 	HashMap<String, User> users;
 	
-    public PlayStore() {
-    	// default constructor
-		this.contents = new HashMap<String, Content>();;
+	public PlayStore() {
+		this.contents = new HashMap<String, Content>();
 		this.users = new HashMap<String, User>();
 	}
-
-    public void addContent(Content content) {
-    	// add new content to content list
-    	this.contents.put(content.contentID, content);
-    }
-    
-       
-    public void addUser(User user) {
-    	// add new user to user list
-    	this.users.put(user.userID, user);
-    }
-        
-	public void showContent() {
-		// show a list of all available contents
-		
-		System.out.println("\nContents of Store\n");
-		for (String i : contents.keySet()) {
-			System.out.println("ID: " + i + " name: " + contents.get(i).name + " price: $" + contents.get(i).price);
-		} 	
+	
+	public void addContent(Content content) {
+		this.contents.put(content.getContentID(), content);
 	}
 	
-	public void showContent(String className) {   	
-		// show list of contents of a particular type such as application, 
-		// book or magazine
-		// className specifies the type
-			
+	public void addUser(User user) {
+		this.users.put(user.getUserID(), user);
+	}
+	
+	public void showContent() {
+		System.out.println("\nContents of Store\n");
+		for(String i : contents.keySet()) {
+			System.out.println("ContentID: " + i + " name: " + contents.get(i).getName() + 
+					" price: $" + contents.get(i).getPrice());
+		}
+	}
+	
+	public void showContent(String className) {
 		System.out.println(className + " Type Contents of Store\n");
-		for (String i : contents.keySet()) {
-		    if(contents.get(i).getClass().getName().equals(className))	// check for type
-		    	System.out.println("ID: " + i + " name: " + contents.get(i).name + " price: $" + contents.get(i).price);
-		} 
+		for(String i : contents.keySet()) {
+			if(contents.get(i).getClass().getName().equals(className))
+				System.out.println("ContentID: " + i + " name: " + contents.get(i).getName() +
+						" price: $" + contents.get(i).getPrice());
+		}
 	}
 }
-
