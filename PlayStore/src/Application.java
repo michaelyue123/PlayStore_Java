8d5202a1da09eca6d5643b050bad5ae44b011c8a
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2019-08-02 15:34:04 +1000

    Update Application.java

--- PlayStore/src/Application.java
+++ PlayStore/src/Application.java
@@ -1,36 +1,35 @@
 import java.util.*;
 
-// create a class Application
 class Application extends Content {
-	// instance variable
-	protected String osType;
+	private String osType;
 	
 	public Application(String contentID, String name, int downNum, double price,
 			ArrayList<Comment> comments, String osType) {
 		super(contentID, name, downNum, price, comments);
-		this.osType = osType;		
-	}
-
-	public Application(String contentID, String name, double price,	String osType) {
-		this(contentID, name, 0, price, new ArrayList<Comment>(),osType);
-	}
-	// overloading + set up the initial value for Application 
-	public Application(String contentID, String name, String osType) {
-		this(contentID, name, 0, 0, new ArrayList<Comment>(), osType);
-	}
-
-	@Override
-	public void addReview(Comment comment) {	
-		this.comments.add(comment); // add comment to Application
-	}
-
-	@Override
-	public ArrayList<Comment> showComments() {
-        return this.comments; // return the list of comments for Application		
+		this.osType = osType;
 	}
 	
-	@Override
-	public void removeReview(Comment comment) {
-		this.comments.remove(comment); // remove comment from Application
+	public Application(String contentID, String name, double price, String osType) {
+		this(contentID, name, 0, price, new ArrayList<Comment>(), osType);
 	}
+	
+	public void addReview(Comment c) {
+		this.getComment().add(c);
+	}
+	
+	public String getOsType() {
+		return osType;
+	}
+	
+	public void setOsType(String newOsType) {
+		this.osType = newOsType;
+	}
+	
+	public ArrayList<Comment> showComments() {
+		return this.getComment();
+	}
+	
+	public void removeReview(Comment c) {
+		this.getComment().remove(c);
+	}	
 }
