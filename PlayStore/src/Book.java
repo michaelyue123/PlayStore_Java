821511b11fd90066e95067ea122b7177e367889f
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2019-08-02 15:34:57 +1000

    Update Book.java

--- PlayStore/src/Book.java
+++ PlayStore/src/Book.java
@@ -1,38 +1,41 @@
 import java.util.*;
 
-//create a class Book
 class Book extends Publication {
-	// subclass instance variables with default modifiers
-	String[] authors;
+	private String[] authors;
 	
-	public Book(String contentID, String name, int downNum, double price, ArrayList<Comment> comments, String publisher,
-			int numPage, String... authors) {
+	public Book(String contentID, String name, int downNum, double price, 
+			ArrayList<Comment> comments, String publisher, int numPage, String...authors) {
 		super(contentID, name, downNum, price, comments, publisher, numPage);
 		this.authors = authors;
 	}
 	
-	// overloading + set up the initial value for Book
-	public Book(String contentID, String name, int downNum, double price, ArrayList<Comment> comments, String publisher,
-			int numPage) {
-		this(contentID, name, 0, price, new ArrayList<Comment>(), publisher, numPage, "");
+	public Book(String contentID, String name, int downNum, double price, 
+			ArrayList<Comment> comments, String publisher, int numPage) {
+		this(contentID, name, downNum, price, new ArrayList<Comment>(), publisher, numPage, "");
 	}
 	
-	public Book(String contentID, String name, double price, String publisher,
-			int numPage,String... authors) {
+	public Book(String contentID, String name, double price, 
+			ArrayList<Comment> comments, String publisher, int numPage, String...authors) {
 		this(contentID, name, 0, price, new ArrayList<Comment>(), publisher, numPage, authors);
 	}
-	@Override
-	public void addReview(Comment comment) {	
-		this.comments.add(comment); // add comment to Book
-	}
-
-	@Override
-	public ArrayList<Comment> showComments() {
-        return this.comments; // return the list of comments for Book		
+	
+	public String[] getAuthors() {
+		return this.authors;
 	}
 	
-	@Override
-	public void removeReview(Comment comment) {
-		this.comments.remove(comment); // remove comment from Book
+	public void setAuthors(String[] newAuthors) {
+		this.authors = newAuthors;
 	}
-}
+	
+	public void addReview(Comment comment) {
+		this.getComment().add(comment);
+	}
+	
+	public ArrayList<Comment> showComments() {
+		return this.getComment();
+	}
+	
+	public void removeReview(Comment comment) {
+		this.getComment().remove(comment);
+	}
+} 
